42de4a016c48bdeb9face10cbc6e52c4dafb0a11
Masbro <s2gotosingapore@gmail.com>
2020-04-20 16:58:26 +0700

    Controller berita

---
+++ app/Http/Controllers/BeritaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\berita;
+use Illuminate\Http\Request;
+
+class BeritaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\berita  $berita
+     * @return \Illuminate\Http\Response
+     */
+    public function show(berita $berita)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\berita  $berita
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(berita $berita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\berita  $berita
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, berita $berita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\berita  $berita
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(berita $berita)
+    {
+        //
+    }
+}
