0b46c1fe30aef6616aef53c67e119158f4c0478e
Masbro <s2gotosingapore@gmail.com>
2020-04-22 00:06:18 +0700

    Visitor C

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\visitor;
+use Illuminate\Http\Request;
+
+class VisitorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, visitor $visitor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\visitor  $visitor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(visitor $visitor)
+    {
+        //
+    }
+}
