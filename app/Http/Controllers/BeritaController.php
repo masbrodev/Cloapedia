fd3f38125f9cc2b1608e975554fc2bbe3a131cb5
Masbro <s2gotosingapore@gmail.com>
2020-04-21 23:32:36 +0700

    Berita controller

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use App\berita;
 use Illuminate\Http\Request;
+use App\Visitor;
+use App\Comment;
+use App\Like;
+use DateTime;
+use DB;
+
+date_default_timezone_set("Asia/Jakarta");
 
 class BeritaController extends Controller
 {
@@ -12,74 +19,95 @@ class BeritaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $date        = new DateTime();
+        $this->month = $date->format('m') - 1;
+        $this->year  = $date->format('Y');
+
+        $this->beritaBaru    = Berita::orderBy('id', 'DESC')->limit(5)->get();
+        $this->beritaPopuler = Berita::orderBy('dilihat', 'DESC')->limit(5)->get();
+    }
+
     public function index()
     {
-        //
+        $beritas     = Berita::orderBy('id', 'DESC')->paginate(3);
+        $slides      = Berita::orderBy('id', 'DESC')->limit(3)->get();
+        $beritaSaran = Berita::whereMonth("created_at", $this->month)->whereYear("created_at", $this->year)->orderBy("dilihat", "DESC")->limit(3)->get();
+
+        return view('pages.home', ['beritas' => $beritas, 'beritaBaru' => $this->beritaBaru, 'beritaPopuler' => $this->beritaPopuler, 'beritaSaran' => $beritaSaran, 'slides' => $slides, 'controller' => $this]);
+        // return $slides;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+    public function showBerita($path)
     {
-        //
+        $berita = Berita::where('path', $path)->first();
+        $berita->dilihat = $berita->dilihat + 1;
+        $berita->save();
+
+        $visitor = new Visitor;
+        $visitor->save();
+
+        $cat = explode(", ", $berita->kategori);
+        $cat = '%' . $cat[0] . '%';
+        $beritaTerkait = Berita::where('kategori', 'LIKE', $cat)->get();
+
+        $comments = DB::table('comments')->where('comments.post_id', $berita->id)
+            ->where('parent_id', '==', 0)
+            ->join('users', 'users.id', '=', 'comments.user_id')
+            ->orderBy('comments.id', 'DESC')->get(['comments.*', 'users.name', 'users.profile']);
+
+        $replies  = DB::table('comments')->where('comments.post_id', $berita->id)
+            ->where('parent_id', '!=', 0)
+            ->join('users', 'users.id', '=', 'comments.user_id')
+            ->orderBy('comments.id', 'DESC')->get(['comments.*', 'users.name', 'users.profile']);
+
+        $likes    = Like::where('post_id', $berita->id)->get();
+
+        return view('berita', ['berita' => $berita, 'beritaBaru' => $this->beritaBaru, 'beritaPopuler' => $this->beritaPopuler, 'beritaTerkait' => $beritaTerkait, 'comments' => $comments, 'replies' => $replies, 'likes' => $likes, 'controller' => $this]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function kategoriBerita($cat)
     {
-        //
+        $cat     = '%' . $cat . '%';
+        $beritas = Berita::where('kategori', 'LIKE', $cat)->get();
+        return view('kategori', ['beritas' => $beritas, 'beritaBaru' => $this->beritaBaru, 'beritaPopuler' => $this->beritaPopuler, 'controller' => $this]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\berita  $berita
-     * @return \Illuminate\Http\Response
-     */
-    public function show(berita $berita)
+    public function cari(Request $request)
     {
-        //
+        $search  = '%' . $request->search . '%';
+        $beritas = Berita::where('judul', 'LIKE', $search)->limit(10)->get();
+        return view('cari', ['beritas' => $beritas, 'beritaBaru' => $this->beritaBaru, 'beritaPopuler' => $this->beritaPopuler, 'msg' => 'Menampilkan hasil pencarian untuk : ' . $request->search, 'controller' => $this]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\berita  $berita
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(berita $berita)
+    /* ------------ Without Routing ------------- */
+
+    public function kategori($cat)
     {
-        //
+        $kategoris = explode(", ", $cat);
+        foreach ($kategoris as $kategori) {
+            $kategoriA = $kategori;
+            $kategori = ($kategori != $kategoris[0]) ? ", " . $kategori : $kategori;
+            echo '<a class="cat-news" href="/kategori/' . $kategoriA . '">' . $kategori . '</a>';
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\berita  $berita
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, berita $berita)
+    public function tanggal($tgl)
     {
-        //
+        $date  = new DateTime($tgl);
+        $month = array('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+        $time  = $date->format('d') . " " . $month[$date->format('m') - 1] . " " . $date->format('Y');
+        echo '<div class="time-news">' . $time . '</div>';
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\berita  $berita
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(berita $berita)
+    public function fullTime($tgl)
     {
-        //
+        $date  = new DateTime($tgl);
+        $month = array('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
+        $time  = $date->format('d') . " " . $month[$date->format('m') - 1] . " " . $date->format('Y') . " " . $date->format("H:i:s");
+        echo '<div class="time-news">' . $time . '</div>';
     }
 }
