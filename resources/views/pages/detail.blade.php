dd06f521739e513f8d9ffaca152a70d11a2487d8
Masbro <s2gotosingapore@gmail.com>
2020-04-25 15:41:39 +0700

    Pembetulan lokasi assets

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -322,7 +322,7 @@
                         <h2 class="widget-title">Advertising</h2>
                         <div class="banner-spot clearfix">
                             <div class="banner-img">
-                                <img src="upload/banner_03.jpg" alt="" class="img-fluid">
+                                <img src="assets/upload/banner_03.jpg" alt="" class="img-fluid">
                             </div><!-- end banner-img -->
                         </div><!-- end banner -->
                     </div><!-- end widget -->
