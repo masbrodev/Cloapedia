d4e0540aab773e933c8414ee9c74c893d8a0d58b
Masbro <s2gotosingapore@gmail.com>
2020-04-26 14:09:07 +0700

    Pemindahan Lokasi Iklan bener

---
+++ resources/views/components/iklanbanner-spot.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-lg-10 offset-lg-1">
+        <div class="banner-spot clearfix">
+            <div class="banner-img">
+                <img src="assets/upload/banner_01.jpg" alt="" class="img-fluid">
+            </div><!-- end banner-img -->
+        </div><!-- end banner -->
+    </div><!-- end col -->
+</div><!-- end row -->
