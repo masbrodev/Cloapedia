071dce08c64ce103f3873a5e7e98790cd93f680c
Masbro <s2gotosingapore@gmail.com>
2020-04-26 14:09:20 +0700

    Include iklan

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,15 +10,8 @@
     <div class="container">
 
         <!-- IKLAN -->
-        <div class="row">
-            <div class="col-lg-10 offset-lg-1">
-                <div class="banner-spot clearfix">
-                    <div class="banner-img">
-                        <img src="assets/upload/banner_01.jpg" alt="" class="img-fluid">
-                    </div><!-- end banner-img -->
-                </div><!-- end banner -->
-            </div><!-- end col -->
-        </div><!-- end row -->
+        @include('components.iklanbanner-spot')
+        <!-- IKLAN -->
 
         <hr class="invis1">
 
@@ -280,15 +273,10 @@
 
         <hr class="invis1">
 
-        <div class="row">
-            <div class="col-lg-10 offset-lg-1">
-                <div class="banner-spot clearfix">
-                    <div class="banner-img">
-                        <img src="assets/upload/banner_02.jpg" alt="" class="img-fluid">
-                    </div><!-- end banner-img -->
-                </div><!-- end banner -->
-            </div><!-- end col -->
-        </div><!-- end row -->
+        <!-- IKLAN -->
+        @include('components.iklanbanner-spot')
+        <!-- IKLAN -->
+
     </div><!-- end container -->
 </section>
 
