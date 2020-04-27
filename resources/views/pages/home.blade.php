74d8031a3374e3b6b3952ab1bc5bb6aac51d027f
Masbro <s2gotosingapore@gmail.com>
2020-04-27 22:43:57 +0700

    tambah berita yang dinamis

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -22,66 +22,49 @@
                         <h3 class="color-green"><a href="blog-category-01.html" title="">Travel</a></h3>
                     </div><!-- end title -->
 
-                    <div class="blog-box row">
-                        <div class="col-md-4">
-                            <div class="post-media">
-                                <a href="single.html" title="">
-                                    <img src="assets/upload/blog_square_01.jpg" alt="" class="img-fluid">
-                                    <div class="hovereffect"></div>
-                                </a>
-                            </div><!-- end media -->
-                        </div><!-- end col -->
+                    @foreach($slides as $slide)
+                    @if($loop->index < 2 AND $slide->kategori = 'Travel')
+                        <div class="blog-box row">
+                            <div class="col-md-4">
+                                <div class="post-media">
+                                    <a href="single.html" title="">
+                                        <img src="assets/img/{{$slide->foto}}" alt="" class="img-fluid">
+                                        <div class="hovereffect"></div>
+                                    </a>
+                                </div><!-- end media -->
+                            </div><!-- end col -->
 
-                        <div class="blog-meta big-meta col-md-8">
-                            <h4><a href="single.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
-                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
-                            <small><a href="blog-category-01.html" title="">Travel</a></small>
-                            <small><a href="single.html" title="">21 July, 2017</a></small>
-                            <small><a href="blog-author.html" title="">by Boby</a></small>
-                        </div><!-- end meta -->
-                    </div><!-- end blog-box -->
+                            <div class="blog-meta big-meta col-md-8">
+                                <h4><a href="single.html" title="">5 Beautiful buildings you need to visit without dying</a></h4>
+                                <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                <small><a href="blog-category-01.html" title="">{{$slide->kategori}}</a></small>
+                                <small><a href="single.html" title="">21 July, 2017</a></small>
+                                <small><a href="blog-author.html" title="">by Boby</a></small>
+                            </div><!-- end meta -->
+                        </div><!-- end blog-box -->
 
-                    <hr class="invis">
+                        <hr class="invis">
+                        @endif
+                        @endforeach
 
-                    <div class="blog-box row">
-                        <div class="col-md-4">
-                            <div class="post-media">
-                                <a href="single.html" title="">
-                                    <img src="assets/upload/blog_square_02.jpg" alt="" class="img-fluid">
-                                    <div class="hovereffect"></div>
-                                </a>
-                            </div><!-- end media -->
-                        </div><!-- end col -->
+                        <div class="blog-box row">
+                            <div class="col-md-4">
+                                <div class="post-media">
+                                    <a href="single.html" title="">
+                                        <img src="assets/upload/blog_square_03.jpg" alt="" class="img-fluid">
+                                        <div class="hovereffect"></div>
+                                    </a>
+                                </div><!-- end media -->
+                            </div><!-- end col -->
 
-                        <div class="blog-meta big-meta col-md-8">
-                            <h4><a href="single.html" title="">Let's make an introduction to the glorious world of history</a></h4>
-                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
-                            <small><a href="blog-category-01.html" title="">Travel</a></small>
-                            <small><a href="single.html" title="">20 July, 2017</a></small>
-                            <small><a href="blog-author.html" title="">by Samanta</a></small>
-                        </div><!-- end meta -->
-                    </div><!-- end blog-box -->
-
-                    <hr class="invis">
-
-                    <div class="blog-box row">
-                        <div class="col-md-4">
-                            <div class="post-media">
-                                <a href="single.html" title="">
-                                    <img src="assets/upload/blog_square_03.jpg" alt="" class="img-fluid">
-                                    <div class="hovereffect"></div>
-                                </a>
-                            </div><!-- end media -->
-                        </div><!-- end col -->
-
-                        <div class="blog-meta big-meta col-md-8">
-                            <h4><a href="single.html" title="">Did you see the most beautiful sea in the world?</a></h4>
-                            <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
-                            <small><a href="blog-category-01.html" title="">Travel</a></small>
-                            <small><a href="single.html" title="">19 July, 2017</a></small>
-                            <small><a href="blog-author.html" title="">by Jackie</a></small>
-                        </div><!-- end meta -->
-                    </div><!-- end blog-box -->
+                            <div class="blog-meta big-meta col-md-8">
+                                <h4><a href="single.html" title="">Did you see the most beautiful sea in the world?</a></h4>
+                                <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                <small><a href="blog-category-01.html" title="">Travel</a></small>
+                                <small><a href="single.html" title="">19 July, 2017</a></small>
+                                <small><a href="blog-author.html" title="">by Jackie</a></small>
+                            </div><!-- end meta -->
+                        </div><!-- end blog-box -->
                 </div><!-- end blog-list -->
 
                 <hr class="invis">
